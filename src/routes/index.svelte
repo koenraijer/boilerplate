e4d1378935d30ea50c8db2ae370134da3407a909
Koen Raijer <koenraijer@gmail.com>
2022-01-15 15:08:39 +0100

    Landing page with projects listed

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -6,30 +6,23 @@
         - the client takes our query and returns data inside of a const.
     */
     import {client} from '$lib/graphql-client'
-    import {gql} from 'graphql-request'
+    import { authorsQuery, projectsQuery } from '$lib/graphql-queries'
 
-    export const load = async () => {
-        // 1. Make a GraphQL query to define the data needed.
-        const query = gql`
-            query GetProjects {
-                projects {
-                name
-                slug
-                description
-                demo
-                sourceCode
-                image {
-                    url
-                }
-                }
-            }
-        `
+    export const load = async () => {        
+
         // 2. Give that query to the GraphQL client.
-        const {projects} = await client.request(query)
+        const [authorsReq, projectsReq] = await Promise.all([
+            client.request(authorsQuery),
+            client.request(projectsQuery),
+        ])
+
+        const {authors} = authorsReq
+        const {projects} = projectsReq 
 
         // 3. Return that data to the <script> tag
         return {
             props: {
+                authors,
                 projects,
             },
         }
@@ -38,10 +31,26 @@
 
 <script>
     // 4. Export the data for use in the page
+    export let authors
     export let projects
 </script>
 
-<h1>Recent projects</h1>
+<svelte:head>
+    <title>Koen codes</title>
+</svelte:head>
+
+<h1 class="font-bold text-center mb-20 text-5xl">Koen codes</h1>
+
+{#each authors as {name, intro, picture: {url} }}
+    <div class="flex mb-40 items-end">
+        <div class="mr-6">
+        <h2 class="text-3xl mb-4 font-bold tracking-wider">{name}</h2>
+        <p class="text-xl mb-4">{intro}</p>
+        </div>
+
+        <img class="mask mask-squircle h-48" src={url} alt={name} />
+    </div>
+{/each}
 
 {#each projects as { name, slug, description, image }}
     <ProjectCard {name} {description} url={image[0].url} {slug} />
